69637425e4fe7ebe7ef20f1d5c02b57ea2e5d4c3
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-03-30 16:52:43 +0530

    adding node docker

--- node-server/app/app.js
+++ node-server/app/app.js
@@ -3,14 +3,21 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
+const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send(Date.now().toString());
+  res.sendStatus(200); 
 });
+
+app.get('/web', (req, res) => {
+  res.send(Date.now().toString());
+  res.sendStatus(200); 
+});
+
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
