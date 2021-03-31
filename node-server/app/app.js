fe6f8fdcfc10e81faf849f96f992cee83ccb948b
Venkatesan Panneerselvam <vselvam@vmware.com>
2021-03-31 07:58:48 +0530

    adding apps for loadbalancing in docker-compose file

--- node-server/app/app.js
+++ node-server/app/app.js
@@ -8,16 +8,10 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.send(Date.now().toString());
   res.sendStatus(200); 
 });
-
-app.get('/web', (req, res) => {
-  res.send(Date.now().toString());
-  res.sendStatus(200); 
-});
-
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
